c3df140da4abaaa77f697002bca539c575047a84
André Maia <amaia@compugraf.com.br>
2014-09-01 20:42:11 -0300

    Complementando o exemplo com validações mais reais

--- ObjectValidation.cs
+++ ObjectValidation.cs
@@ -13,146 +13,202 @@ using System.Linq;
 namespace Csharp_playground
 {
 
-	/// <summary>
-	/// Classes de modelo.
-	/// </summary>
-	namespace Model
-	{
-		/// <summary>
-		/// Representa uma pessoa
-		/// </summary>
-		class Pessoa
-		{
-			String name;
+    /// <summary>
+    /// Classes de modelo.
+    /// </summary>
+    namespace Model
+    {
+        /// <summary>
+        /// Representa uma pessoa
+        /// </summary>
+        class Pessoa
+        {
+            public string Name { get; set; }
 
-			public Pessoa(String name)
-			{
-				this.name = name;
-			}
+            public Pessoa(string name)
+            {
+                this.Name = name;
+            }
+        }
 
-			public String getName()
-			{
-				return name;
-			}
-		}
+        /// <summary>
+        /// Representa uma pessoa física
+        /// </summary>
+        class PessoaFisica : Pessoa
+        {
+            public string Cpf { get; set; }
 
-		/// <summary>
-		/// Representa uma pessoa física
-		/// </summary>
-		class PessoaFisica: Pessoa
-		{
-			public PessoaFisica(String name)
-				: base(name)
-			{
-			}
-		}
+            public PessoaFisica(String name, string cpf)
+                : base(name)
+            {
+                this.Cpf = cpf;
+            }
 
-		/// <summary>
-		/// Representa uma pessoa jurídica
-		/// </summary>
-		class PessoaJuridica: Pessoa
-		{
-			public PessoaJuridica(String name)
-				: base(name)
-			{
-			}
-		}
-	}
+        }
 
-	namespace Validations
-	{
+        /// <summary>
+        /// Representa uma pessoa jurídica
+        /// </summary>
+        class PessoaJuridica : Pessoa
+        {
+            public string Cnpj { get; set; }
 
+            public PessoaJuridica(String name, string cnpj)
+                : base(name)
+            {
+                this.Cnpj = cnpj;
+            }
+        }
+    }
 
-		/// <summary>
-		/// Interface para permitir que objetos sejam validados.
-		/// </summary>
-		interface IValidatable<T> where T: class
-		{
-			void Validate(T o) ;
-		}
+    namespace Validations
+    {
 
-		/// <summary>
-		/// Validação para pessoa.
-		/// </summary>
-		class PessoaValidator: IValidatable<Pessoa>
-		{
+        /// <summary>
+        /// Interface base de validação.
+        /// </summary>
+        interface IValidatable
+        {
+            Dictionary<object, string> ValidationMessages { get; set; }
+            void Validate(object o);
+        }
 
-			public void Validate(Pessoa o)
-			{
-				Console.WriteLine(o.getName() + '-' + this.GetType().Name);
-			}
-		}
+        /// <summary>
+        /// Interface para permitir que objetos sejam validados.
+        /// </summary>
+        abstract class Validatable<T> : IValidatable where T : class
+        {
+            public Dictionary<object, string> ValidationMessages { get; set; }
+            internal abstract void Validate(T o);
 
-		/// <summary>
-		/// Validação para uma pessoa física
-		/// </summary>
-		class PessoaFisicaValidator: IValidatable<PessoaFisica>
-		{
+            public Validatable()
+            {
+                ValidationMessages = new Dictionary<object, string>();
+            }
 
-			public void Validate(PessoaFisica o)
-			{
-				Console.WriteLine(o.getName() + '-' + this.GetType().Name);
-			}
-		}
+            public void Validate(object o)
+            {
+                this.Validate(o as T);
+            }
+        }
 
-		/// <summary>
-		/// Validacão para um pessoa jurídica
-		/// </summary>
-		class PessoaJuridicaValidator: IValidatable<PessoaJuridica>
-		{
-			public void Validate(PessoaJuridica o)
-			{
-				Console.WriteLine(o.getName() + '-' + this.GetType().Name);
-			}
-		}
+        /// <summary>
+        /// Validação para pessoa.
+        /// </summary>
+        class PessoaValidator : Validatable<Pessoa>
+        {
 
-		/// <summary>
-		/// Estratégias de validação
-		/// </summary>
-		class ValidatorStrategy
-		{
-			Dictionary<Type, object> map = null;
+            internal override void Validate(Pessoa o)
+            {
+                Console.WriteLine(o.Name + '-' + this.GetType().Name);
+            }
+        }
 
-			public ValidatorStrategy()
-			{
+        /// <summary>
+        /// Validação para uma pessoa física
+        /// </summary>
+        class PessoaFisicaValidator : Validatable<PessoaFisica>
+        {
 
-				// Cadastro dos vários tipos de validadores
-				// para cara tipo de entidade.
-				map = new Dictionary<Type, object>();
-				map.Add(typeof(Pessoa), new PessoaValidator());
-				map.Add(typeof(PessoaFisica), new PessoaFisicaValidator());
-				map.Add(typeof(PessoaJuridica), new PessoaJuridicaValidator());
-			}
-
-			public IValidatable<T> Get<T>() where T: class
-			{
-				return map[typeof(T)] as IValidatable<T>;
-			}
-		}
-	}
+            internal override void Validate(PessoaFisica o)
+            {
+                Console.WriteLine(o.Name + '-' + this.GetType().Name);
+                checkCpf(o);
+            }
 
 
-	/// <summary>
-	/// Exemplo de validação de objeto.
-	/// </summary>
-	public class ObjectValidation
-	{
-		public void Main()
-		{
-			var pessoas = new List<Pessoa>();
-			pessoas.Add(new Pessoa("Uma pessoa simples"));
-			pessoas.Add(new PessoaFisica("Joãoo"));
-			pessoas.Add(new PessoaFisica("José"));
-			pessoas.Add(new PessoaJuridica("Maria"));
+            void checkCpf(PessoaFisica o)
+            {
+                // "333.333.333-33"
+                if (o.Cpf.Length != 13)
+                    base.ValidationMessages.Add(this, "O CPF não contém o tamanho correto.");
+            }
 
-			var validator = new ValidatorStrategy();
-			foreach (var p in pessoas) {
-				var v = validator.Get(p.GetType());
-				v.Validate(p);
+        }
 
-			}
+        /// <summary>
+        /// Validacão para um pessoa jurídica
+        /// </summary>
+        class PessoaJuridicaValidator : Validatable<PessoaJuridica>
+        {
+            internal override void Validate(PessoaJuridica o)
+            {
+                Console.WriteLine(o.Name + '-' + this.GetType().Name);
+                isCnpjValid(o);
+            }
 
-		}
-	}
+            void isCnpjValid(PessoaJuridica o)
+            {
+                //  99.999.999/9999-99
+                if (o.Cnpj.Length != 18)
+                    base.ValidationMessages.Add(this, "O tamanho do CNPJ é inválido");
+            }
+        }
+
+        /// <summary>
+        /// Estratégias de validação
+        /// </summary>
+        class ValidatableStrategy
+        {
+            Dictionary<Type, IValidatable> map = null;
+
+            public ValidatableStrategy()
+            {
+
+                // Cadastro dos vários tipos de validadores
+                // para cara tipo de entidade.
+                map = new Dictionary<Type, IValidatable>();
+                map.Add(typeof(Pessoa), new PessoaValidator());
+                map.Add(typeof(PessoaFisica), new PessoaFisicaValidator());
+                map.Add(typeof(PessoaJuridica), new PessoaJuridicaValidator());
+            }
+
+            public IValidatable Get(Type validateFor)
+            {
+                return map[validateFor] as IValidatable;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Exemplo de validação de objeto.
+    /// </summary>
+    public class ObjectValidation
+    {
+        public void Main()
+        {
+            var pessoas = new List<Pessoa>();
+            pessoas.Add(new Pessoa("PessoaBase"));
+            pessoas.Add(new PessoaFisica("João", "333.333.33-33"));
+            pessoas.Add(new PessoaFisica("José", "333.333.333-33"));
+            pessoas.Add(new PessoaJuridica("Maria", "999.99/9999-99"));
+
+
+            var strategies = new ValidatableStrategy();
+
+            foreach (var p in pessoas)
+            {
+                var validator = strategies.Get(p.GetType());
+                validator.Validate(p);
+
+
+                if (validator.ValidationMessages.Any())
+                {
+                    Console.WriteLine("Mensagens de validação:");
+                    foreach (var item in validator.ValidationMessages)
+                        Console.WriteLine("\t{0} - {1}", item.Key.GetType().Name, item.Value);
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+
+
+
+        }
+    }
 }
 
